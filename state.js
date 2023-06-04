6e8bd3869fd7fc9f7e82be7b1aeaf53f1fda0cb7
jaimytaco <jaimy.taco@gmail.com>
2023-06-04 16:24:21 -0500

    feat: state for step 3,4,6,12

--- state.js
+++ state.js
@@ -13,26 +13,17 @@ const updateAffiliation = (step) => {
     localStorage.setItem('affilliaton', updatedAffliationString)
 }
 
-/*
-
-{
-    member: 'me',
-    name: 'Angelo',
-    age: 12
-}
-
-{ member: 'me'} + { name: 'gbhnjm,'} = {}
-
-
-*/
+const isStep = (step) => location.pathname.includes(step)
 
 const initStep0 = () => {
+    if (!isStep('welcome.html')) return
+
     const affiliation = getAffiliation()
     if (affiliation) {
         const chosenMember = affiliation.member
         const chosenBtn = document.querySelector(`a.btn-big-icon.btn-shift[data-affiliation-member=${chosenMember}]`)
-        if (!chosenBtn) return
-        chosenBtn.classList.add('active')
+        // if (!chosenBtn) return
+        chosenBtn?.classList.add('active')
     }
     
     const btn = document.querySelector('a[href="step-01.html"]')
@@ -50,6 +41,8 @@ const initStep0 = () => {
 }
 
 const initStep1 = () => {
+    if (!isStep('step-01.html')) return
+
     const nameInput = document.getElementById('name')
     if (!nameInput) return
 
@@ -73,6 +66,8 @@ const initStep1 = () => {
 }
 
 const initStep2 = () => {
+    if (!isStep('step-02.html')) return
+
     const affiliation = getAffiliation()
     if (affiliation){
         const helloElement = document.getElementById('hello')
@@ -92,13 +87,88 @@ const initStep2 = () => {
     if (!btn) return
     btn.addEventListener('click', (event) => {
         const ageElement = document.querySelector('.age-picker .picker .choice.chosen')
-        console.log('ageElement =', ageElement)
         const age = ageElement.textContent
         
         updateAffiliation({ age })
     })
 }
 
+const initStep3 = () => {
+    if (!isStep('step-03.html')) return
+
+    const affiliation = getAffiliation()
+    if (affiliation) {
+        const chosenSmoke = affiliation.smoke
+        const chosenBtn = document.querySelector(`a.btn-big-icon.btn-shift[data-affiliation-smoke=${chosenSmoke}]`)
+        chosenBtn?.classList.add('active')
+    }
+    
+    const btn = document.querySelector('a[href="step-04.html"]')
+    if (!btn) return
+    btn.addEventListener('click', (event) => {
+        const active = document.querySelector('a.btn-big-icon.btn-shift.active')
+        if (!active){
+            event.preventDefault()
+            return
+        }
+
+        const smoke = active.getAttribute('data-affiliation-smoke')
+        updateAffiliation({ smoke })
+    })
+}
+
+const initStep4 = () => {
+    if (!isStep('step-04.html')) return
+
+    const affiliation = getAffiliation()
+    if (affiliation) {
+        const chosenIllness = affiliation.illness
+        const chosenBtn = document.querySelector(`a.btn-big-icon.btn-shift[data-affiliation-illness=${chosenIllness}]`)
+        chosenBtn?.classList.add('active')
+    }
+    
+    const btn = document.querySelector('a[href="step-06.html"]')
+    if (!btn) return
+    btn.addEventListener('click', (event) => {
+        const active = document.querySelector('a.btn-big-icon.btn-shift.active')
+        if (!active){
+            event.preventDefault()
+            return
+        }
+
+        const illness = active.getAttribute('data-affiliation-illness')
+        updateAffiliation({ illness })
+    })
+}
+
+const initStep6 = () => {
+    if (!isStep('step-06.html')) return
+
+    const affiliation = getAffiliation()
+    if (affiliation) {
+        const chosenCommunication = affiliation.communication
+        const chosenBtn = document.querySelector(`a.btn-big-icon.btn-shift[data-affiliation-communication=${chosenCommunication}]`)
+        chosenBtn?.classList.add('active')
+    }
+    
+    const btn = document.querySelector('a[href="step-10.html"]')
+    if (!btn) return
+    btn.addEventListener('click', (event) => {
+        const active = document.querySelector('a.btn-big-icon.btn-shift.active')
+        if (!active){
+            event.preventDefault()
+            return
+        }
+
+        const communication = active.getAttribute('data-affiliation-communication')
+        updateAffiliation({ communication })
+    })
+}
+
+
 initStep0()
 initStep1()
 initStep2()
+initStep3()
+initStep4()
+initStep6()
